ac0077a7bcbe3f0eb8bfd2186c965799c23ad385
hokageCV <chaitanyavaru@gmail.com>
2023-07-17 02:08:22 +0530

    protect url

--- background/background.js
+++ background/background.js
@@ -1,19 +1,25 @@
-console.log("background is running");
-
 chrome.runtime.onInstalled.addListener(async (e) => {
     if (e.reason === chrome.runtime.OnInstalledReason.INSTALL) {
-        console.log("installed");
-
-        /**
-         * @param {string} redirectURL - URL for redirection
-         */
         await chrome.storage.local.set({ redirectUrl: "" });
-
-        /**
-         * @param {string[]} keywords - array of keywords
-         */
         await chrome.storage.local.set({ keywords: [] });
     } else if (e.reason === "update") {
         console.log("Antauri Sprx! Gibson! Nova! Otto! REELOD!");
+    }
+});
+
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+    const { keywords, redirectUrl } = await chrome.storage.local.get(["keywords", "redirectUrl"]);
+
+    if (changeInfo.status === "complete" && tab.url) {
+        const matchedKeyword = keywords.find((keyword) => tab.url.includes(keyword));
+
+        if (matchedKeyword) {
+            if (redirectUrl === "" || redirectUrl === undefined) {
+                return chrome.tabs.update(tabId, {
+                    url: "https://chaitanya-varu-blog.netlify.app",
+                });
+            }
+            chrome.tabs.update(tabId, { url: redirectUrl });
+        }
     }
 });
