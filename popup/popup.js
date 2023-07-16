e6acc90c3c4750c70add1b0173b29e77f3569130
hokageCV <chaitanyavaru@gmail.com>
2023-07-16 23:40:12 +0530

    add render list

--- popup/popup.js
+++ popup/popup.js
@@ -1,6 +1,6 @@
 const redirectUrlInput = document.getElementById("redirect-url-input");
 const addKeywordInput = document.getElementById("add-keyword-input");
-const keywordList = document.getElementById("keyword-list");
+const keywordListDiv = document.getElementById("keyword-list");
 
 document.addEventListener("DOMContentLoaded", () => {
     renderKeywordList();
@@ -20,7 +20,54 @@ redirectUrlInput.addEventListener("keydown", async (e) => {
     }
 });
 
+addKeywordInput.addEventListener("keydown", async (e) => {
+    let keyCode = e.code || e.key;
+    if (keyCode === "Enter") {
+        e.preventDefault();
+
+        if (addKeywordInput.value) {
+            chrome.storage.local.get(["keywords"], (data) => {
+                const keywordsList = data.keywords || [];
+                keywordsList.push(addKeywordInput.value);
+
+                chrome.storage.local.set({ keywords: keywordsList }, () => {
+                    addKeywordInput.value = "";
+                    renderKeywordList();
+                });
+            });
+        }
+    }
+});
+
 // =========================================================
+function renderKeywordList() {
+    chrome.storage.local.get(["keywords"], (data) => {
+        const keywordsList = data.keywords || [];
+
+        keywordListDiv.innerHTML = "";
+        keywordsList.forEach((keyword) => {
+            const listItem = document.createElement("li");
+            listItem.classList.add("keyword-list-item");
+            listItem.textContent = keyword;
+
+            const deleteBtn = document.createElement("button");
+            deleteBtn.innerHTML = "X";
+
+            deleteBtn.addEventListener("click", () => {
+                const newKeywordsList = keywordsList.filter((item) => item !== keyword);
+                chrome.storage.local.set({ keywords: newKeywordsList }, () => {
+                    renderKeywordList();
+                });
+            });
+
+            listItem.appendChild(deleteBtn);
+            keywordListDiv.appendChild(listItem);
+        });
+    });
+}
+
+// =========================================================
+
 function isURL(str) {
     try {
         new URL(str);
